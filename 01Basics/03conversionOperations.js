d9e4b22a55f2d846ab0ce970a490be1b3b99f05f
ishitasriv <73216580+ishitasriv@users.noreply.github.com>
2023-07-24 18:00:38 +0000

    operationConevrsation

--- 01Basics/03conversionOperations.js
+++ 01Basics/03conversionOperations.js
@@ -1,4 +1,4 @@
-let score = null
+/*  let score = null
 console.log(typeof score)
 let valueInNumber = Number (score)
 console.log(typeof valueInNumber)
@@ -13,4 +13,21 @@ console.log(booleanisLoggedIn)
 //"pppp" ~ true
 let someNumber =33
 let StringsomeNumber =String(someNumber)
-console.log(typeof StringsomeNumber)
+console.log(typeof StringsomeNumber)*/
+
+let value =3
+let negValue = - value
+console.log(negValue)
+let str1="hello"
+let str2=" Ishita"
+let str3 =str1+ str2
+
+console.log(1+ 3 + "4")// output on the basis of 1st data type
+console.log("3"+ 2 + 2)
+let num1, num2, num3
+num1 = num2=num3 =2+2
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter)
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
