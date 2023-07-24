a65c062ab54d572fadc353ae02575c69dd12b86f
ishitasriv <73216580+ishitasriv@users.noreply.github.com>
2023-07-24 17:09:18 +0000

    JS Basics

---
+++ 01Basics/02DataTypes.js
@@ -0,0 +1,15 @@
+"use strict"; // treat all JS code as new version
+//alert(3+3)//we are using node js not browser
+// code readability should be high
+//documentation MDN reference or ecma script
+let name ="Ishita"
+let age = 16
+let isLoggedIn = false
+//number ~ 2 to power 53
+// bigint storing large ints
+// string ""/''
+// booleam ~ true/false
+//null datatype + standalone value
+//undefined 
+// symbol
+console.log(typeof name)
