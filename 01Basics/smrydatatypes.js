794809141ce576fbc94754a53ae7b3f223ef8a9c
ishitasriv <73216580+ishitasriv@users.noreply.github.com>
2023-07-24 19:15:44 +0000

    Data Types Summary

---
+++ 01Basics/smrydatatypes.js
@@ -0,0 +1,19 @@
+// Primitive (call by value)
+//7 types
+// String, Boolean, Number, null, undefinned, Symbol, BigInt
+const score = 100
+const scoreValue =100.3
+const isLoggedIn=false
+const outsideTemp = null
+let userEmail;
+const id = Symbol('123')
+const anotherid =Symbol('123')
+console.log(id==anotherid)
+const bigNumber = 123n
+//Reference Type (non primitive)
+//Array, Objects, Functions
+const heros = ["Batman","IronMan","Witcher"]
+let myObj = {
+    name : Ishita,
+    age : 16
+}//key: value pair {} 
