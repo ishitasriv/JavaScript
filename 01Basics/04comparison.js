78b1e7338ddaebf04bafc1553abf861642907511
ishitasriv <73216580+ishitasriv@users.noreply.github.com>
2023-07-24 18:05:47 +0000

    comparison

---
+++ 01Basics/04comparison.js
@@ -0,0 +1,8 @@
+console.log(2 > 1)
+console.log ("2">1)
+console.log("02">1)// don't get predictable results
+console.log (null>0)
+console.log (null == 0)
+console.log (null >=0)// avoid not clean code
+
+console.log(undefined ==0)
