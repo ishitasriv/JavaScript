a32a361d0ff941dce411107a5f9e98374fd3993b
ishitasriv <73216580+ishitasriv@users.noreply.github.com>
2023-07-28 14:25:35 +0000

    Numbers and Maths

---
+++ 01Basics/06NumsnMaths.js
@@ -0,0 +1,23 @@
+const score = 400
+/*const balance = new Number(100)
+console.log(balance)
+console.log(balance.toString())
+console.log(typeof(balance))
+console.log(balance.toFixed(2))
+const otherN =123.8966
+console.log(otherN.toPrecision(4))
+const hun =1000000
+console.log(hun.toLocaleString('en-IN'))*/
+//MATHS
+/*console.log(Math)
+console.log(Map.round(4.6))
+console.log(Math.round(-4))
+console.log(Math.floor(4.9))
+//console.log(Math.min(array))
+//console.log(Math.max(array))
+console.log(Math.random())
+console.log((MAth.random()*10)+1)
+console.log(Math.random()*10+1)*/
+const min =10
+const max = 20
+console.log(Math.floor(Math.random()* (max-min+1))+min)
