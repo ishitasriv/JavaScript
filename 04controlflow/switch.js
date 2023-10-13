ffe8581de87eadf1ffbc1a46997d0f1d818a3b00
ishitasriv <73216580+ishitasriv@users.noreply.github.com>
2023-10-13 22:04:49 +0000

    switch

---
+++ 04controlflow/switch.js
@@ -0,0 +1,27 @@
+/*
+switch(key){
+    case value:
+
+        break;
+    default:
+
+        break;
+}*/
+const month = 'Jan'
+switch(month){
+    case 'Jan':
+        console.log("January");
+        break;
+    case 'Feb':
+        console.log("February");
+        break;
+    case 'Mar':
+        console.log("March");
+        break;
+    case 'Apr':
+        console.log("April");
+        break;
+    default:
+        console.log('not the first 4');
+        break;
+    }
