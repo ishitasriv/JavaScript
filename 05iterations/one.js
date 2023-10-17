aa19cfc9e81441b3cf64de9fe24e3d1243f6d48d
ishitasriv <73216580+ishitasriv@users.noreply.github.com>
2023-10-17 14:59:08 +0000

    for loop

--- 05iterations/one.js
+++ 05iterations/one.js
@@ -1,7 +1,30 @@
 //for
-for (let i  = 0; i  < 10 ; i++) {
+/*for (let i  = 0; i  < 10 ; i++) {
+    console.log(i);
     if (i==5){
         console.log("5 is best");
     }
+}*/
+for(let i =0;i<=10;i++){
+    //console.log(i);
+    for (let j = 0; j <=10; j++) {
+        //console.log(i+'*'+j +'='+ i*j);
+        
+    }
+}
+let myArray=['flash','batman','superman']
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    //console.log(element);
     
 }
+// break and continue
+for (let index = 1; index <=20; index++) {
+    if (index==5){
+        console.log(`${index} Detected`);
+        continue;
+    }
+    console.log(`Value of i is ${index}`);
+    
+}
+
